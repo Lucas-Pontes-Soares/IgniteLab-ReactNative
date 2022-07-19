3b4a1fa63df077488b975d52199116a0987654bb
Lucas <lucas.soares177@etec.sp.gov.br>
2022-07-19 11:42:03 -0300

    Terminando primeira aula

--- rockethelp/src/components/Input.tsx
+++ rockethelp/src/components/Input.tsx
@@ -10,6 +10,11 @@ export function Input({...rest}: IInputProps) {
         fontFamily="body" 
         color="white" 
         placeholderTextColor="gray.300"
+        _focus={{
+          borderWidth: 1,
+          borderColor: "green.500",
+          bg: "gray.700"
+        }}
         {...rest}
     />
   );
