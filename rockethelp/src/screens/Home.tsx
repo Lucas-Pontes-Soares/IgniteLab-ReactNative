02a9fd419866f5863ee03da17ed4af8b5b01ff83
Lucas <lucas.soares177@etec.sp.gov.br>
2022-07-26 11:15:47 -0300

    Adicionando rotas

--- rockethelp/src/screens/Home.tsx
+++ rockethelp/src/screens/Home.tsx
@@ -53,11 +53,11 @@ export function Home() {
         <VStack flex={1} px={6}>
             <HStack w="full" mt={8} justifyContent="space-between" alignItems="center">
                 <Heading color="gray.100">
-                    Meus chamados
+                    Solicitações
                 </Heading>
 
                 <Text color="gray.200">
-                    3
+                    {orders.length}
                 </Text>
             </HStack>
 
